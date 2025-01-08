5201100b9a1467b087447b34cdc2e9ed2069fc7d
Mohin Uddin <mmishrat90@gmail.com>
2025-01-08 15:14:01 +0600

    'doctro'

--- backend/healthcare/authApi/serializers.py
+++ backend/healthcare/authApi/serializers.py
@@ -1,7 +1,8 @@
 from rest_framework import serializers
+from django.contrib.auth.models import User
 from .models import Doctor , Patient, Designation ,Specialization , Availebletime
 class doctorSerializer(serializers.ModelSerializer) :
-    user = serializers.StringRelatedField(many = False)
+    user = serializers.PrimaryKeyRelatedField(queryset=User.objects.all())
     designation = serializers.StringRelatedField(many = True)
     specialization = serializers.StringRelatedField(many = True)
     availabletime = serializers.StringRelatedField(many =True)
