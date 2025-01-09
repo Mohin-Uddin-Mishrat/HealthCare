e807c1c5ac0c00bbaf585bff3b33dd61d32c0597
Mohin Uddin <mmishrat90@gmail.com>
2025-01-09 12:07:12 +0600

    '.'

--- backend/healthcare/othersApi/serializers.py
+++ backend/healthcare/othersApi/serializers.py
@@ -1,4 +1,5 @@
 from .models import contactUsModel , serviceModel , appointmentModel, reviewModel
+from authApi.models import Doctor ,Availebletime
 from rest_framework import serializers
 from django.contrib.auth.models import User
 class contactUsSerializers(serializers.ModelSerializer):
@@ -12,9 +13,12 @@ class serviceSerializers(serializers.ModelSerializer):
         fields = '__all__'
 
 class appointmentSerializers(serializers.ModelSerializer):
-    doctor= serializers.StringRelatedField(many=False)
-    user= serializers.StringRelatedField(many=False)
-    time= serializers.StringRelatedField(many=False)
+    doctor = serializers.SlugRelatedField(
+        queryset=Doctor.objects.all(), 
+        slug_field='user__username'  # Use the username from the related User model
+    )
+    user=serializers.SlugRelatedField(queryset=User.objects.all(), slug_field='username') 
+    time= serializers.SlugRelatedField(queryset=Availebletime.objects.all(), slug_field='name')  
     class Meta :
         model = appointmentModel
         fields = '__all__'
